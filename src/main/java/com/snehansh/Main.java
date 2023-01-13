87577de21c397d3e96a673c9a77ecc5e8302c3dd
Shekhar <dev.snehansh@gmail.com>
2023-01-14 02:05:00 +0530

    xml based configuration of spring boot to handle dependeny injuction

--- src/main/java/com/snehansh/Main.java
+++ src/main/java/com/snehansh/Main.java
@@ -1,8 +1,18 @@
 package com.snehansh;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Doctor doctor = context.getBean(Doctor.class);
+        Nurse nurse = context.getBean(Nurse.class);
         doctor.assist();
+        nurse.assist();
+        Staff staff = context.getBean(Nurse.class);
+        staff.assist();
+        System.out.println(doctor.getQualification());
+        System.out.println(nurse.getQualification());
     }
 }
--- src/main/java/com/snehansh/Nurse.java
+++ src/main/java/com/snehansh/Nurse.java
@@ -1,19 +1,17 @@
 package com.snehansh;
 
-public class Doctor implements Staff{
+public class Nurse implements Staff{
     private String qualification;
-    Doctor(String qualification){
-        this.qualification = qualification;
-    }
+
     public String getQualification() {
         return qualification;
     }
-    
+
     public void setQualification(String qualification) {
         this.qualification = qualification;
     }
 
     public void assist(){
-        System.out.println("Doctor is assisting");
+        System.out.println("Nurse is assisting");
     }
 }
