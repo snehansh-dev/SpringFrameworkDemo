56fbb6ede401bb4b320c90616c579443343e9c6e
Shekhar <dev.snehansh@gmail.com>
2023-01-14 02:58:38 +0530

    Beans using java class

--- src/main/java/com/snehansh/Main.java
+++ src/main/java/com/snehansh/Main.java
@@ -1,18 +1,20 @@
 package com.snehansh;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Doctor doctor = context.getBean(Doctor.class);
+      //  ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+      //  Doctor doctor = context.getBean(Doctor.class);
         Nurse nurse = context.getBean(Nurse.class);
-        doctor.assist();
+        //doctor.assist();
         nurse.assist();
         Staff staff = context.getBean(Nurse.class);
         staff.assist();
-        System.out.println(doctor.getQualification());
+        //System.out.println(doctor.getQualification());
         System.out.println(nurse.getQualification());
     }
 }
